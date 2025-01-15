5d16646b785b7a37c7b2dca078c00123cb12c77b
damianayala <damian.villarreal-ayala@menloschool.org>
2025-01-15 13:46:09 -0800

    1st commit

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date    |  Time  |                                                                                                             Update |
+|:--------|:------:|-------------------------------------------------------------------------------------------------------------------:|
+| April 5 | 9-10pm |                                                                         I tried to write abstract, efficient code. |
+| Jan 15  | 12-1pm | I experimented and thought about different approaches, including a recursive method before I shied away from that. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -3,12 +3,36 @@
  * each day’s temperature is higher than on the previous day in that sequence.
  *
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Damian Villarreal-Ayala
+ *
+ * Notes:
+ * You want to find the longest span of days in which each day’s temperature is higher than on the previous day in that
+ * sequence. The days you choose don’t have to be consecutive in the original list, but the order must be preserved.
+ * ESSENTIALLY, you're making a consective list of higher temperatures, and you're allowed to ignore some on your way.
+ *
+ *
+ * Window/Trunkated approach:
+ * - Checking a window of 2, then 3, then etc.
+ * - If I want some more support on this approach, ask Mr. Blick
+ *
+ * Assigning numbers their "trend" value and assign values the further in you go?
+ * Ex.
+ * 21, 25, 15, 23, 24
+ * 1 ,  2,  1,  2, ?
+ * You would give 24 a 3 for their "trend" value, but have it built off of the [15, 23] since
+ * they have the same "trend" value as [21, 25] which remaining a lower digit.
+ * Staying with the lower digit gives us more room to add on to the trend tracker.
+ *
+ * Would using a recursive method be a good idea?
+ * Whenever we come across a new value that would "reset" the trend tracker, recurse and find the longest trend of
+ * that section. Maybe we could go back and compare each respective trend tracker?
+ * Look more into how a recursive method would work..
+ *
+ * Recursive method likely not the best
+ *
  */
 
 public class WeatherPatterns {
-
-
     /**
      * Longest Warming Trend
      * @param temperatures
@@ -16,7 +40,44 @@ public class WeatherPatterns {
      */
     public static int longestWarmingTrend(int[] temperatures) {
         // TODO: Write your code here!
+        //Creating a highest point and lowest point right now just to have the variables in case I need them.
+        int pointBreak = temperatures[0];
+        int highestPoint = 0;
+        int trendLength = 1;
 
-        return 0;
+        for(int i = 0; i < temperatures.length; i++)
+        {
+            //To check output and help visualize
+            System.out.println(temperatures[i]);
+//            if(temperatures[i] < lowestPoint)
+//            {
+//                lowestPoint = temperatures[i];
+//                trendLength = 1;
+//            }
+            //If the current temperature is less than or equal to pointBreak, skip over it.
+//            if (pointBreak >= temperatures[i])
+//            {
+//                i++;
+//            }
+//            else if (pointBreak < temperatures[i])
+//            {
+//                trendLength++;
+//                pointBreak = temperatures[i];
+//            }
+
+
+
+
+//            if(temperatures[i] > lowestPoint)
+//            {
+//                trendLength++;
+//            }
+//            else if (temperatures[i] <= lowestPoint)
+//            {
+//                i++;
+//            }
+        }
+
+        return trendLength;
     }
 }
